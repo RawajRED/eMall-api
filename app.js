7d625c24f32198bb9650464a34d808007d112897
asserhamad <asserhamad96@gmail.com>
2021-06-27 17:06:54 +0200

    added cors

--- app.js
+++ app.js
@@ -13,8 +13,16 @@ const {getVariables, changeVariables} = require('./variables');
 
 require('dotenv').config();
 const app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 app.set('secret_key', process.env.SECRET_KEY);
 app.use(cors());
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
